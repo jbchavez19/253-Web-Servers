3a63e7de51f03d3cc7ff666904f99db4de9235ed
SomayaB <somaya.bounouar@gmail.com>
2017-04-18 12:03:52 -0700

    Implement Save button

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const marked = require('marked');
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.text());
 app.set('view engine', "pug");
 app.set("views","views");
 
@@ -16,6 +17,11 @@ app.use(express.static('public'));
 app.get("/", (req, res) => {
   res.render("Index");
 });
+
+app.post("/save", (req, res) => {
+ res.send(req.body)
+ //create file with request body
+})
 
 
 app.listen(port, () => {
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -5,6 +5,26 @@ let updatePreview = () => {
   document.querySelector(".preview").innerHTML = markdown
 }
 
+let save = () => {
+  let editorInput = document.getElementById("editor").value
+  fetch('/save', {
+    method: 'post',
+    headers: {
+      "Content-Type": "text/plain"
+    },
+    body: editorInput
+  })
+  .then((response) => {
+    return response.text()
+  })
+  .then((response) => {
+    console.log(response)
+  })
+  .catch(console.log)
+
+}
+
 window.onload = () => {
   document.getElementById("editor").addEventListener("input", updatePreview)
+  document.getElementById("save").addEventListener("click", save)
 }
